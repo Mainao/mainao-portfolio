d92084054c933002be9d9e291b15a1572efbe1c3
Mainao Boro <mainao1230@gmail.com>
2026-01-05 23:59:04 +0530

    add gsap animation

--- components/GSAPAnimations.tsx
+++ components/GSAPAnimations.tsx
@@ -8,9 +8,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function GSAPAnimations() {
     useGSAP(() => {
-        gsap.to(".hero-content", { opacity: 1, duration: 1 });
-        gsap.to(".flower-images", { opacity: 1, duration: 1 });
-        gsap.to(".flower", { rotate: 360, duration: 2 });
+        gsap.to(".hero-content", {
+            opacity: 1,
+            duration: 1,
+            ease: "power1.inOut",
+        });
+        gsap.to(".flower-images", {
+            opacity: 1,
+            duration: 1,
+            ease: "power1.inOut",
+        });
+        gsap.to(".flower", { rotate: 360, duration: 2, ease: "power2.inOut" });
+        gsap.to("#hero", {
+            scrollTrigger: {
+                trigger: "#experience",
+                start: "top bottom",
+                end: "top bottom-=400",
+                scrub: true,
+            },
+            opacity: 0,
+            ease: "power1.out",
+        });
 
         document.querySelectorAll(".flower").forEach((flower) => {
             flower.addEventListener("mouseenter", () =>
